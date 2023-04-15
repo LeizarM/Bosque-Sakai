dbe549a6435ab04443de554b98f64dd9363f74f0
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2023-04-15 11:48:00 -0400

    adding personal data, but change the presentations

--- src/app/protected/ficha-trabajador/ficha-trabajador.module.ts
+++ src/app/protected/ficha-trabajador/ficha-trabajador.module.ts
@@ -1,15 +1,37 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PrimeNgModule } from 'src/app/prime-ng/prime-ng.module';
+import { DatosPersonalesComponent } from './pages/datos-personales/datos-personales.component';
 import { FichaTrabajadorComponent } from './pages/ficha-trabajador/ficha-trabajador.component';
+import { CiExpedidoPipe } from './pipes/ci-expedido.pipe';
+import { EstadoCivilPipe } from './pipes/estado-civil.pipe';
+import { FormacionPipe } from './pipes/formacion.pipe';
+import { GeneroPipe } from './pipes/genero.pipe';
+import { PaisPipe } from './pipes/pais.pipe';
+import { TipoDuracionPipe } from './pipes/tipo-duracion.pipe';
 
 
 
 @NgModule({
   declarations: [
-    FichaTrabajadorComponent
+
+    CiExpedidoPipe,
+    EstadoCivilPipe,
+    FormacionPipe,
+    GeneroPipe,
+    PaisPipe,
+    TipoDuracionPipe,
+
+
+
+    FichaTrabajadorComponent,
+    DatosPersonalesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PrimeNgModule,
+    ReactiveFormsModule
   ]
 })
 export class FichaTrabajadorModule { }
