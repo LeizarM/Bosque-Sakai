785a7ae3717b2bed1d70b4565af73e8921f6e886
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2023-05-05 12:29:09 -0400

    adding more interfaces to prices modules's

---
+++ src/app/protected/interfaces/GrupoFamiliaSap.ts
@@ -0,0 +1,8 @@
+export interface GrupoFamiliaSap {
+  idGrpFamiliaSap?: number;
+  codGrpFamSap?:    number;
+  codGrpFamSapEpp?: number;
+  grpFam?:          string;
+  alias?:           string;
+  audUsuario?:      number;
+}
