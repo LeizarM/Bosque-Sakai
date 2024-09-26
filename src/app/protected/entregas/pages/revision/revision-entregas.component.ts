6fafdf652a2aa8cc29ba0719f7054f052691285f
Marcelo Jaimes <marceloeziojaimes@gmail.com>
2024-09-26 10:51:13 -0400

    refactor: Update RevisionEntregasComponent CSS and HTML files

--- src/app/protected/entregas/pages/revision/revision-entregas.component.ts
+++ src/app/protected/entregas/pages/revision/revision-entregas.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import maplibregl from 'maplibre-gl';
 import { EntregaChofer } from 'src/app/protected/interfaces/EntregaChofer';
+import { MapaLibreService } from 'src/app/protected/mapaLibre/mapaLibre.service';
 import { RevisionEntregasService } from '../../services/revision-entregas.service';
 
 @Component({
@@ -7,25 +9,50 @@ import { RevisionEntregasService } from '../../services/revision-entregas.servic
   templateUrl: './revision-entregas.component.html',
   styleUrls: ['./revision-entregas.component.css']
 })
-export class RevisionEntregasComponent implements OnInit {
-
+export class RevisionEntregasComponent implements OnInit, AfterViewInit {
+  @ViewChild('map') mapElement!: ElementRef;
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
   filterDate: Date | string = "";
-
-
-  lstEntregasRegistradas : EntregaChofer[]= [];
+  lstEntregasRegistradas: EntregaChofer[] = [];
+  map!: maplibregl.Map;
+  selectedEntrega: EntregaChofer | null = null;
 
   constructor(
-    private revisionEntregaService : RevisionEntregasService
+    private revisionEntregaService: RevisionEntregasService,
+    private mapaLibreServices: MapaLibreService
   ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.initializeMap();
+    this.adjustMapSize();
   }
 
+  @HostListener('window:resize')
+  onResize() {
+    this.adjustMapSize();
+  }
+
+  initializeMap() {
+    this.map = new maplibregl.Map({
+      container: this.mapElement.nativeElement,
+      style: this.mapaLibreServices.obtenerZonaxCiudad(),
+      center: [-68.1500, -16.5000],
+      zoom: 12
+    });
+  }
+
+  adjustMapSize() {
+    if (this.mapContainer && this.mapContainer.nativeElement) {
+      const containerHeight = window.innerHeight - this.mapContainer.nativeElement.getBoundingClientRect().top - 20;
+      this.mapContainer.nativeElement.style.height = `${containerHeight}px`;
+      this.map.resize();
+    }
+  }
 
   onBuscar() {
-
     if (this.filterDate) {
       this.revisionEntregaService.obtenerEntregasXFecha(this.filterDate).subscribe({
         next: (res) => {
@@ -38,19 +65,28 @@ export class RevisionEntregasComponent implements OnInit {
     }
   }
 
+  onVerEntrega(entrega: EntregaChofer): void {
+    this.selectedEntrega = entrega;
+    if (entrega.longitud && entrega.latitud) {
+      this.map.flyTo({
+        center: [entrega.longitud, entrega.latitud],
+        zoom: 15
+      });
 
-  onVerEntrega( entrega : EntregaChofer ) : void {
+      const existingMarkers = document.getElementsByClassName('maplibregl-marker');
+      while(existingMarkers[0]) {
+        if (existingMarkers[0] && existingMarkers[0].parentNode) {
+          existingMarkers[0].parentNode.removeChild(existingMarkers[0]);
+        }
+      }
 
-    console.log(entrega);
+      new maplibregl.Marker()
+        .setLngLat([entrega.longitud, entrega.latitud])
+        .addTo(this.map);
+    }
 
+    setTimeout(() => {
+      this.adjustMapSize();
+    }, 0);
   }
-
-
-
-
-
-
-
-
-
 }
