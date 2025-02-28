1542966de5cf6fdf5faa5b3549d982b7f1288108
Marcelo Javier Jaimes <marceloeziojaimes@gmail.com>
2025-02-28 19:21:16 -0400

    feat: Update environment baseUrl, enhance login component styling, and improve form validation messages

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //baseUrl: 'http://192.168.3.116:9223'
   //baseUrl: 'http://192.168.3.107:9223'
-  baseUrl: 'http://181.114.119.195:9334'
+  baseUrl: 'https://mleizar.lat:8443'
 
 };
